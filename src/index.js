b8a9f1df47810416e4c480f4150e11d6e97bc650
Roberto A S Zanin <raszanin@gmail.com>
2022-03-21 15:45:14 -0300

    Funcionalidades

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json())
+
+const projects = [];
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World"})
@@ -10,42 +13,61 @@ app.get("/", (request, response) => {
 
 app.get("/projects", (request, response) => {
 
-  const { title, owner} = request.query;
+  const { title } = request.query;
 
-  console.log(title);
-  console.log(owner);
+  const result = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects
 
-  return response.json([
-    "Projeto 1",
-    "Projeto 2"
-  ])
+  return response.json(result)
 })
 
 app.post("/projects", (request, response) => {
 
-  const { title, owner}  = request.body;
+  const { title, owner } = request.body;
 
-  return response.json({ title, owner })
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project)
 })
 
 app.put("/projects/:id", (request, response) => {
 
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id)
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    "Projeto 1",
-    "Projeto 2",
-    "Projeto 4"
-  ])
+  if (projectIndex < 0) {
+    return response.status(400).json({ erro: "Project not found"})
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(project)
 })
 
 app.delete("/projects/:id", (request, response) => {
-  return response.json([
-    "Projeto 2",
-    "Projeto 4"
-  ])
+  
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ erro: "Project not found"})
+  }
+
+  projects.splice(projectIndex, 1);
+  
+  return response.status(204).send()
 })
 
 
